77d3092024db363b98c36497cb12a0b7956e8b09
Weilian Song <weiliansong@hotmail.com>
2016-02-26 23:32:04 -0500

    Forgot to save the file

--- src/networking/TextProtocol.java
+++ src/networking/TextProtocol.java
@@ -2,15 +2,20 @@ package networking;
 
 public class TextProtocol {
 	private static final int WAITING = 0;
-	private static final int MSG_SENT = 1;
-	private static final int MSG_NOT_SENT = 2;
-	private static final int DISCONNECTED = 3;
+	private static final int MSG_QUEUED = 1;
+	private static final int MSG_SENT = 2;
+	private static final int MSG_NOT_SENT = 3;
+	private static final int DISCONNECTED = 4;
 	
 	private int state = WAITING;
 	
 	public String processInput(String input) {
-		String output = NULL;
+		String output = null;
 		
+		if(state == WAITING) {
+			
+		}
 		
+		return output;
 	}
 }
